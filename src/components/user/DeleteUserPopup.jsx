07988505894bea426c40cde9daf70fb5a1361c33
okramnesseam <58548477+okramnesseam@users.noreply.github.com>
2021-04-16 13:27:27 +0300

    another clean

--- src/components/user/DeleteUserPopup.jsx
+++ src/components/user/DeleteUserPopup.jsx
@@ -1,15 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import useDeleteUser from '../../graphql/user/hooks/useDeleteUser'
 
 
 export default function DeleteUserPopup(props) {
-    console.log("MOI")
     const { open, handleClose, user } = props
     const [deleteUser] = useDeleteUser()
 
